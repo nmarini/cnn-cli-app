349617c188f8982218614df3867a021b8aa87c14
Nicolas Marini <nmarini44@gmail.com>
2018-07-31 22:26:10 +0000

    creating menu

--- lib/cnn_cli_app/cli.rb
+++ lib/cnn_cli_app/cli.rb
@@ -2,7 +2,18 @@ class CNNCLIApp::CLI
 
   def call
     # put all of the methods you make in this method
-    puts "NEWS"
+    puts "Welcome to the CNN CLI App!"
+    menu
+
+  end
+
+  def menu
+    puts "Please choose which category to view.  Choose 1, 2, or 3:"
+    puts <<-DOC
+    1. CNN ANALYSIS
+    2. TODAY IN POLITICS
+    3. TOP STORIES
+    DOC
   end
 
 end
