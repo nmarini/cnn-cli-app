f2b2c2e619eb772cc27298333207b4374b907746
Nicolas Marini <nmarini44@gmail.com>
2018-07-31 22:46:08 +0000

    creating menu and categories methods in CLI

--- lib/cnn_cli_app/cli.rb
+++ lib/cnn_cli_app/cli.rb
@@ -3,13 +3,12 @@ class CNNCLIApp::CLI
   def call
     # put all of the methods you make in this method
     puts "Welcome to the CNN CLI App!"
-    categories
     menu
 
   end
 
   def categories
-    puts "Please choose which category to view.  Choose 1, 2, or 3:"
+    puts "News Story Categories"
     puts <<-DOC
     1. CNN ANALYSIS
     2. TODAY IN POLITICS
@@ -18,7 +17,21 @@ class CNNCLIApp::CLI
   end
 
   def menu
-    
-  end 
+    input = nil
+    until input == "exit"
+      categories
+      puts "Please enter a number that corresponds to your category choice:"
+      input = gets.strip.downcase
+      if input == "1"
+        puts "list of CNN ANALYSIS stories"
+      elsif input == "2"
+        puts "list of TODAY IN POLITICS stories"
+      elsif input == "3"
+        puts "list of TOP STORIES"
+      else
+        puts "Please enter a number that corresponds to your category choice:"
+      end
+    end
+  end
 
 end
