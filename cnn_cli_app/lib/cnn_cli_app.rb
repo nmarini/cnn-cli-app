d05ff211db812c37b43dd67afa3abc9525ff9ee9
Nicolas Marini <nmarini44@gmail.com>
2018-07-31 21:53:48 +0000

    merging local and remote

---
+++ cnn_cli_app/lib/cnn_cli_app.rb
@@ -0,0 +1,12 @@
+require "cnn_cli_app/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./cnn_cli_app/version"
+require_relative './cnn_cli_app/deal'
+require_relative './cnn_cli_app'
+
+module CnnCliApp
+  # Your code goes here...
+end
---
+++ cnn_cli_app/lib/cnn_cli_app/cli.rb
@@ -0,0 +1,7 @@
+class CNN::CLI
+
+  def call
+    # put all of the methods you make in this method
+  end
+
+end 
