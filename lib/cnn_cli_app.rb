adc7daab30a1241e4ac689fed9f3734b2edb61cf
Nicolas Marini <nmarini44@gmail.com>
2018-08-01 00:06:21 +0000

    fixing bugs in gemspec file

--- lib/cnn_cli_app.rb
+++ lib/cnn_cli_app.rb
@@ -1,8 +1,7 @@
-# require 'open-uri'
-# require 'nokogiri'
-# require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
 require_relative './cnn_cli_app/version'
 require_relative './cnn_cli_app/story'
-require_relative './cnn_cli_app'
 require_relative './cnn_cli_app/cli'
--- lib/cnn_cli_app/story.rb
+++ lib/cnn_cli_app/story.rb
@@ -1,8 +1,14 @@
+
+
 class CNNCLIApp::Story
   attr_accessor :title, :author, :description, :category
 
   @@stories = []
 
+  def self.scrape_analysis
+    doc = Nokogiri::HTML(open("https://www.cnn.com/?refresh=1"))
+binding.pry
+    story = self.new
+  end
 
-
-end 
+end
