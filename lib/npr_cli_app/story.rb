487eae7f0a79288131c40a0be96baf6a9fc3525d
Nicolas Marini <nmarini44@gmail.com>
2018-08-02 22:39:34 +0000

    added scrape_top

--- lib/npr_cli_app/story.rb
+++ lib/npr_cli_app/story.rb
@@ -12,9 +12,9 @@ class NPRCLIApp::Story
   def self.scrape_top
     doc = Nokogiri::HTML(open("https://www.npr.org"))
     top_story = NPRCLIApp::Story.new
-    top_story.title =
-    top_story.category =
-    top_story.link =
+    top_story.title = doc.search('h3.title').first.text
+    top_story.category = doc.search('h2.slug a').first.text.strip
+    top_story.link = doc.search('div.story-text a')[1].attr('href')
   binding.pry
   end
 
