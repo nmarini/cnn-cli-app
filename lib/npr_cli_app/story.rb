014ebf86e71f9830c33dd12d457c09c0a1432c6e
Nicolas Marini <nmarini44@gmail.com>
2018-08-02 22:45:33 +0000

    each enumerator for featured_stories

--- lib/npr_cli_app/story.rb
+++ lib/npr_cli_app/story.rb
@@ -9,20 +9,23 @@ class NPRCLIApp::Story
     @@stories
   end
 
-  def self.scrape_top
+  def self.scrape_top_stories
     doc = Nokogiri::HTML(open("https://www.npr.org"))
     top_story = NPRCLIApp::Story.new
     top_story.title = doc.search('h3.title').first.text
     top_story.category = doc.search('h2.slug a').first.text.strip
     top_story.link = doc.search('div.story-text a')[1].attr('href')
-  binding.pry
+
   end
 
   def self.scrape_featured
     doc = Nokogiri::HTML(open("https://www.npr.org"))
 
+    featured_stories = doc.search('section.featured-group')
+    featured_stories.each do |article|
+      story = NPRCLIApp::Story.new
 
-
+  binding.pry
   end
 
 end
