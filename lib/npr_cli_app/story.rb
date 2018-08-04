3a1fc8e61357dce37608d7ac805eacf1d8405c75
Nicolas Marini <nmarini44@gmail.com>
2018-08-04 21:11:18 +0000

    added category attr back in, also finished scrape featured

--- lib/npr_cli_app/story.rb
+++ lib/npr_cli_app/story.rb
@@ -1,7 +1,7 @@
 
 
 class NPRCLIApp::Story
-  attr_accessor :title, :link#, :category
+  attr_accessor :title, :link, :category
 
   @@stories = []
 
@@ -13,26 +13,27 @@ class NPRCLIApp::Story
     doc = Nokogiri::HTML(open("https://www.npr.org"))
     top_story = NPRCLIApp::Story.new
     top_story.title = doc.search('h3.title').first.text
-    # top_story.category = doc.search('h2.slug a').first.text.strip
+    top_story.category = doc.search('h2.slug a').first.text.strip
     top_story.link = doc.search('div.story-text a')[1].attr('href')
-
+  binding.pry
   end
 
   def self.scrape_featured
+    scrape_stories = []
     doc = Nokogiri::HTML(open("https://www.npr.org"))
 
     featured_stories = doc.search('section.featured-group')
-    featured_stories.each do |article|
-      article.search('a').each do |s|
-
-      story = NPRCLIApp::Story.new
-
-      # story.title = s.children[1].attr('alt')
-      # story.link = s.attr('href')
-
-      binding.pry
+    featured_stories.search('div.story-wrap').each do |article|
+        story = NPRCLIApp::Story.new
+        
+        story_title = article.search('h3').text
+        story_category = article.search('h2').text.strip
+        story_link = article.search('a').attr('href').value
+        scrape_stories << {title: story_title, link: story_link, category: story_category}
+        
     end
-    end
+    scrape_stories
+    
   end
 
 end
