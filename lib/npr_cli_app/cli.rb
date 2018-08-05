926b19470963b451c2bbcfc4a93238bf1e6c4d97
Nicolas Marini <nmarini44@gmail.com>
2018-08-05 19:17:08 +0000

    completed cli- abandoned all_stories, and utilized specific scrapers

--- lib/npr_cli_app/cli.rb
+++ lib/npr_cli_app/cli.rb
@@ -2,42 +2,64 @@ class NPRCLIApp::CLI
 
   def call
     puts "Welcome to the NPR CLI App! Get the most up-to-date news here from NPR!"
-    top_story
     category_menu
     goodbye
 
   end
 
   def top_story
-  NPRCLIApp::Story.scrape_top_story.title
+    puts "NPT's Top Story:"
+  NPRCLIApp::Story.scrape_top_story.tap{|top_story| puts "#{top_story.title}"}
 
   end
 
   def list_categories
-
-  t =  NPRCLIApp::Story.all_stories
-binding.pry
+    puts "News Categories:"
+   NPRCLIApp::Story.scrape_featured.each.with_index(1){|story, i| puts "#{i}. #{story.category}"}
   end
 
-  def add_stories
-    NPRCLIApp::Story.add_stories
+  # def add_stories
+  #   NPRCLIApp::Story.add_stories
+  # end
+
+  def instructions
+    puts "Please enter"
+    puts "- a number that corresponds to your choice"
+    puts "OR"
+    puts "- type \"top story\" to view NPR's Top Story"
+    puts "OR"
+    puts "- type \"exit\" to leave:"
   end
 
   def category_menu
     input = nil
-    add_stories
+    # add_stories
+    top_story
     list_categories
     until input == "exit"
-      puts "Please enter a number that corresponds to your category choice or type \"exit\" to leave:"
+    instructions
       input = gets.strip.downcase
       if input == "1"
-        puts "List of NPR ANALYSIS stories"
+        puts "#{NPRCLIApp::Story.scrape_featured[0].category}"
+        puts "#{NPRCLIApp::Story.scrape_featured[0].title}"
+        puts "#{NPRCLIApp::Story.scrape_featured[0].link}"
       elsif input == "2"
-        puts "list of TODAY IN POLITICS stories"
+        puts "#{NPRCLIApp::Story.scrape_featured[1].category}"
+        puts "#{NPRCLIApp::Story.scrape_featured[1].title}"
+        puts "#{NPRCLIApp::Story.scrape_featured[1].link}"
       elsif input == "3"
-        puts "list of TOP STORIES"
+        puts "#{NPRCLIApp::Story.scrape_featured[2].category}"
+        puts "#{NPRCLIApp::Story.scrape_featured[2].title}"
+        puts "#{NPRCLIApp::Story.scrape_featured[2].link}"
+
+      elsif input == "top story"
+        puts "#{NPRCLIApp::Story.scrape_top_story.category}"
+        puts "#{NPRCLIApp::Story.scrape_top_story.title}"
+        puts "#{NPRCLIApp::Story.scrape_top_story.link}"
       else
-        puts "Please enter a number that corresponds to your category choice:"
+        top_story
+        list_categories
+        instructions
       end
     end
   end
--- lib/npr_cli_app/story.rb
+++ lib/npr_cli_app/story.rb
@@ -40,3 +40,4 @@ class NPRCLIApp::Story
   end
 
 end
+1
