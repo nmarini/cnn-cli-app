08e55b74892291498e575ba8cbb053c1e5a9db95
Nicolas Marini <nmarini44@gmail.com>
2018-08-06 22:40:56 +0000

    finished menu: correctly returns stories of category input / top story information

--- lib/npr_cli_app/cli.rb
+++ lib/npr_cli_app/cli.rb
@@ -9,20 +9,22 @@ class NPRCLIApp::CLI
 
   def call
     puts "Welcome to the NPR CLI App! Get the most up-to-date news here from NPR!"
+    puts "-----------"
+    top_story
+    puts "-----------"
+    puts "Here is a list of News Categories:"
     category_menu
     goodbye
 
   end
 
   def top_story
-    puts "NPT's Top Story:"
+    puts "NPR's Top Story:"
   NPRCLIApp::Story.scrape_top_story.tap{|top_story| puts "#{top_story.title}"}
 
   end
 
   def list_categories
-    
-    puts "News Categories:"
    NPRCLIApp::Story.scrape_featured.each{|story| @@categories << story.category.downcase}
    @@categories.uniq.each.with_index(1){|category, i| puts "#{i}. #{category.upcase}" if category =~ /[a-z]/i}
   end
@@ -34,21 +36,16 @@ class NPRCLIApp::CLI
   def instructions
     puts "ENTER:"
     puts "- \"LIST\"  to review the list of categories"
-    puts "- The name of the category to view it's stories"
-    
-    # a number that corresponds to your choice"
-    # puts "OR"
-    puts "- type \"top story\" to view NPR's Top Story"
-    puts "OR"
-    puts "- tpe \"list\" to review the list of categories"
-    puts "OR"
-    puts "- type \"exit\" to leave:"
+    puts "- \"The NAME of the CATEGORY\" to view stories related to that category"
+    puts "- \"top story\" to view NPR's Top Story"
+    puts "- \"list\" to review the list of categories"
+    puts "- \"instructions\" to review these instructions"
+    puts "- \"exit\" to leave"
     
   end
 
   def category_menu
     input = nil
-    # add_stories
     list_categories
     instructions
     until input == "exit"
@@ -58,31 +55,22 @@ class NPRCLIApp::CLI
         i = 1
         NPRCLIApp::Story.scrape_featured.each do |story| 
           if story.category.downcase == input
-            puts "#{i}. #{category}: #{story.title}"
+            puts "#{i}. #{story.category}: #{story.title}"
             puts "Teaser: #{story.teaser}"
             puts "Link: #{story.link}"
             puts "-------------"
             i += 1
           end
         end 
-      #   puts "#{NPRCLIApp::Story.scrape_featured[].category}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[0].title}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[0].link}"
-      # elsif input == "2"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[1].category}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[1].title}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[1].link}"
-      # elsif input == "3"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[2].category}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[2].title}"
-      #   puts "#{NPRCLIApp::Story.scrape_featured[2].link}"
 
       elsif input == "top story"
-        puts "#{NPRCLIApp::Story.scrape_top_story.title}"
-        puts "#{NPRCLIApp::Story.scrape_top_story.category}"
-        puts "#{NPRCLIApp::Story.scrape_top_story.teaser}"
-        puts "#{NPRCLIApp::Story.scrape_top_story.link}"
-        
+        title = NPRCLIApp::Story.scrape_top_story.title
+        category = NPRCLIApp::Story.scrape_top_story.category
+        teaser = NPRCLIApp::Story.scrape_top_story.teaser
+        link = NPRCLIApp::Story.scrape_top_story.link
+        puts "#{category}: #{title}"
+        puts "Story: #{teaser}"
+        puts "Link: #{link}"       
 
       elsif input == "list"
         list_categories
